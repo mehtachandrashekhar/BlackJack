4c3ca3513ad326ea7b226809f4980dc4ef42987f
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-05-08 21:54:48 +0530

    code updated

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let secondCard = 9;
 let sum = firstCard + secondCard;
 
 if(sum < 21){
-    console.log("Do you want to draw a new card?")
+    console.log("Do you want to draw a new card?😉")
 }
 else if(sum === 21){
- console.log("Wohooo!! You've BlackJack !")   
+ console.log("Wohooo! You've BlackJack !🔥")   
+}
+else if(sum > 21){
+    console.log("You're out of the game !🥲")
 }
