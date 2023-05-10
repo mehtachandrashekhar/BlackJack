e94c1ea124639ef628a8188478205d50a8d7ddd3
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-05-10 22:29:13 +0530

    code added

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let firstCard = 6;
 let secondCard = 9;
 let sum = firstCard + secondCard;
+let hasBlackJack = false
 
 if(sum < 21){
     console.log("Do you want to draw a new card?😉")
 }
 else if(sum === 21){
- console.log("Wohooo! You've BlackJack !🔥")   
+ console.log("Wohooo! You've BlackJack !🔥")
+ hasBlackJack = true;  
 }
 else {
     console.log("You're out of the game !🥲")
