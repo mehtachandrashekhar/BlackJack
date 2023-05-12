0d385d2033f5cbc13add9b230838eed286a7f6a3
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-05-12 07:57:38 +0530

    code updated

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ let isAlive = true
 let message = ""
 message = "We're logging out"
 if(sum <=21){
-    console.log("Do you want to draw a new card?😉")
+    message = "Do you want to draw a new card?😉"
 }
 else if(sum === 21){
- console.log("Wohooo! You've BlackJack !🔥")
+ message = "Wohooo! You've BlackJack !🔥"
  hasBlackJack = true;  
 }
 else {
-    console.log("You're out of the game !🥲")
+    message = "You're out of the game !🥲"
     isAlive = false 
 }
