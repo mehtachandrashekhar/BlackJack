a8be774aab8631084557e345e6a2396c488e66e4
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-05-19 20:43:22 +0530

    code updated

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 message = "We're logging out"
-if(sum <=21){
-    message = "Do you want to draw a new card?😉"
-}
-else if(sum === 21){
- message = "Wohooo! You've BlackJack !🔥"
- hasBlackJack = true;  
-}
-else {
-    message = "You're out of the game !🥲"
-    isAlive = false 
-}
 
-console.log(message)
+function startGame(){
+    if(sum <=21){
+        message = "Do you want to draw a new card?😉"
+    }
+    else if(sum === 21){
+     message = "Wohooo! You've BlackJack !🔥"
+     hasBlackJack = true;  
+    }
+    else {
+        message = "You're out of the game !🥲"
+        isAlive = false 
+    }
+    
+    console.log(message)
+}
