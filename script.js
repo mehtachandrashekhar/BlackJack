7342ad035b60ca05ee331e17cefa0c8baaa8991a
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-05-19 21:18:34 +0530

    code updated

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let hasBlackJack = false
 let isAlive = true
 let message = "";
 let messageEl = document.getElementById('message-el');
-// let sumEl = document.getElementById('sum-el');
-let sumEl = document.querySelector("#sum-el");
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.querySelector("#cards-el");
 
 
 function startGame(){
