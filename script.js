c3af3b537a5e76a94b0b23bd7ec3e2a22eae4bf1
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-05-21 01:08:32 +0530

    code updated

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ function startGame(){
 
 function renderGame(){
 
-    cardsEl.textContent = "Cards: " + cards[0] +" "+ cards[1];
+    cardsEl.textContent = "Cards: ";
+    for(let i=0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] +" ";
+    }
     sumEl.textContent = "Sum: " + sum;
     if(sum < 21){
         message = "Do you want to draw a new card?"
